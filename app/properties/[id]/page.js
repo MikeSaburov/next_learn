3e91e4525fb415bc3de1957fef9541a17648f9ca
Mikhail Saburov <saburov1985@gmail.com>
2024-05-05 21:52:08 +1100

    изучить хуки usePathname()

--- app/properties/[id]/page.js
+++ app/properties/[id]/page.js
@@ -1,16 +1,18 @@
 'use client';
 
-import { useParams, useSearchParams } from 'next/navigation';
+import { useParams, useSearchParams, usePathname } from 'next/navigation';
 
 export const PropertyIdPage = () => {
   const params = useParams();
   const searchParams = useSearchParams();
+  const pathName = usePathname();
   const name = searchParams.get('name');
 
   return (
     <div>
       <h1>Page {params.id}</h1>
       <p>Params name: {name}</p>
+      <h2>{pathName}</h2>
     </div>
   );
 };
