90b7089c02d3f4b6911a46421f916ab926c37be8
Mikhail Saburov <saburov1985@gmail.com>
2024-05-06 23:00:29 +1100

    импортировать Navbar

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from 'next/font/google';
 import '@/assets/styles/globals.css';
 
+import Navbar from '@/components/Navbar';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
