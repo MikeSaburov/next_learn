c0a2d77f8bc6d072b01734e9da7a38504ea9aae4
Mikhail Saburov <saburov1985@gmail.com>
2024-05-10 00:27:59 +1100

    импортировал Footer и InfoBoxes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Footer from '../components/Footer';
 import Hero from '../components/Hero';
+import InfoBoxes from '../components/InfoBoxes';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <InfoBoxes />
+      <Footer />
     </main>
   );
 }
