0b12d977c518b25c6a82261cef1d7dc9ba602883
Mikhail Saburov <saburov1985@gmail.com>
2024-05-10 00:07:59 +1100

    убрал нинюю границу навбара

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   const [isProfileOpen, setIsProfileOpen] = useState(false);
 
   return (
-    <nav className="bg-emerald-400 border-b border-blue-500">
+    <nav className="bg-emerald-400 ">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-20 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
