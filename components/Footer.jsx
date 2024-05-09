c547a288b9fa622eea3ca6bfe26a5f5bab40ef91
Mikhail Saburov <saburov1985@gmail.com>
2024-05-10 00:47:01 +1100

    верстка Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,13 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="bg-gray-100 py-4 mt-20">
+      <div className="container mx-auto text-center px-4">
+        <p>&copy; {currentYear} Аренда Недвижимостию Все права защищены.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
