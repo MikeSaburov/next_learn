4900ac907ebfc58cdba122fdf85295948a6e87b9
Mikhail Saburov <saburov1985@gmail.com>
2024-05-10 00:40:21 +1100

    верстка компонентов

--- components/InfoBoxes.jsx
+++ components/InfoBoxes.jsx
@@ -5,7 +5,29 @@ const InfoBoxes = () => {
     <section>
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <InfoBox />
+          <InfoBox
+            heading={'для арендаторов'}
+            buttonInfo={{
+              text: 'Подобрать недвижимость',
+              link: '/properties',
+              backgroundCilor: 'bg-amber-300',
+            }}
+          >
+            Найдите недвижимость вашей мечты. Добавьте объекты в закладки и
+            свяжитесь с нами
+          </InfoBox>
+          <InfoBox
+            heading={'для арендодателей'}
+            backgroundColor="bg-emerald-100"
+            buttonInfo={{
+              text: 'Подобрать недвижимость',
+              link: '/properties',
+              backgroundCilor: 'bg-amber-300',
+            }}
+          >
+            Составьте список своей недвижимости и свяжитесь с потенциальными
+            арендаторами. Сдавайте в аренду на короткий или на длительный срок.
+          </InfoBox>
         </div>
       </div>
     </section>
